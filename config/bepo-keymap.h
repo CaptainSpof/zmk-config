b9b23975a0525afdbc68ebf815fb1574a325b0c7
CaptainSpof <captain.spof@gmail.com>
2022-01-06 17:12:56 +0100

    feat(keymap): update bépo & edit behavior
    - Finally figured out how to map a new layout -> fixed erroneous mapping
    - Edit some behavior to match my preference (quick release related)

--- config/bepo-keymap.h
+++ config/bepo-keymap.h
@@ -13,20 +13,20 @@
  */
 // Row 1
 #define _DOL      GRAVE              // $
-#define _DQUOT    LS(SINGLE_QUOTE)   // "
-#define _LESS     LS(COMMA)          // <
-#define _GREAT    LS(DOT)            // >
-/* #define _LGUIL    EQUAL              // ´ (dead) */
-/* #define _RGUIL    EQUAL              // ´ (dead) */
-#define _LBRA     LS(NUMBER_9)       // (
-#define _RBRA     LS(NUMBER_0)       // )
-#define _AT       LS(NUMBER_2)       // @
-#define _PLUS     LS(EQUAL)          // +
-#define _MINUS    MINUS              // -
-#define _SLASH    SLASH              // /
-#define _AST      LS(NUMBER_8)       // *
-#define _EQUAL    EQUAL              // =
-#define _PERC     LS(NUMBER_5)       // %
+#define _DQUOT    N1                 // "
+#define _LESS     RA(N2)             // <
+#define _GREAT    RA(N3)             // >
+#define _LGUIL    N2                 // «
+#define _RGUIL    N3                 // »
+#define _LBRA     N4                 // (
+#define _RBRA     N5                 // )
+#define _AT       N6                 // @
+#define _PLUS     N7                 // +
+#define _MINUS    N8                 // -
+#define _SLASH    N9                 // /
+#define _AST      N0                 // *
+#define _EQUAL    MINUS              // =
+#define _PERC     EQUAL              // %
 
 // Row 2
 #define _B        Q                  // B
@@ -82,17 +82,17 @@
  * └─────┴────┴─────┴───────────────────────┴─────┴────┴─────┘
  */
 // Row 1
-#define _HASH     LS(NUMBER_3)      // #
-#define _1        NUMBER_1          // 1
-#define _2        NUMBER_2          // 2
-#define _3        NUMBER_3          // 3
-#define _4        NUMBER_4          // 4
-#define _5        NUMBER_5          // 5
-#define _6        NUMBER_6          // 6
-#define _7        NUMBER_7          // 7
-#define _8        NUMBER_8          // 8
-#define _9        NUMBER_9          // 9
-#define _0        NUMBER_0          // 0
+#define _HASH     LS(GRAVE)       // #
+#define _1        LS(N1)          // 1
+#define _2        LS(N2)          // 2
+#define _3        LS(N3)          // 3
+#define _4        LS(N4)          // 4
+#define _5        LS(N5)          // 5
+#define _6        LS(N6)          // 6
+#define _7        LS(N7)          // 7
+#define _8        LS(N8)          // 8
+#define _9        LS(N9)          // 9
+#define _0        LS(N0)          // 0
 #define _DEG      LS(_SS)         // ?
 #define _GRAV     LS(_ACUT)       // ` (dead)
 // Row 2
@@ -103,7 +103,20 @@
 /* #define _RABK     LS(_LABK)       // > */
 /* #define _SCLN     LS(_COMM)       // ; */
 /* #define _COLN     LS(_DOT)        // : */
-/* #define _UNDS     LS(_MINS)       // _ */
+#define _UNDS     RA(SPC)       // _
+
+#define _LSBRA     RA(N4)       // [
+#define _RSBRA     RA(N5)       // ]
+#define _LCBRA     RA(X)       // [
+#define _RCBRA     RA(C)       // ]
+
+
+
+
+
+
+
+
 
 /* /\* Alted symbols (Mac) */
 /*  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬─────┐ */
